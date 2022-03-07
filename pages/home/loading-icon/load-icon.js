ee8b0cfa02cf20246ac90320d67d307b62de9929
Muazat Kabara <muazat.kabara1999@gmail.com>
2022-03-07 23:18:28 +0100

    first commit

---
+++ pages/home/loading-icon/load-icon.js
@@ -0,0 +1,14 @@
+//this displays my logo before loading the page
+
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+         document.getElementById('contents').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+           document.getElementById('interactive');
+           document.getElementById('load').style.visibility="hidden";
+           document.getElementById('contents').style.visibility="visible";
+        },1000);
+    }
+}
